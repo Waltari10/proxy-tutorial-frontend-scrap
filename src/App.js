33feb9d37e10608f7a56af709d31d9e1ac07a6c8
valtteriL <valtteri.laine@takamaki.fi>
2019-11-09 17:36:35 +0200

    init

--- src/App.js
+++ src/App.js
@@ -6,18 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            fetch('api/hello-world')
+            .then(res => {
+              console.log(res);
+            })
+          
+          }}
         >
-          Learn React
-        </a>
+          Say hello!
+        </button>
       </header>
     </div>
   );
